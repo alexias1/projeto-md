0cb7d8134cafc53e1de6dbbf12ea95795a5a4afb
alexias1 <arso1@aluno.ifal.edu.br>
2024-11-05 07:55:20 -0300

    implementação da biblioteca gmp, para trabalhar com números maiores, todos os cases funcionando

--- backend/main.c
+++ backend/main.c
@@ -1,93 +1,58 @@
 #include <stdio.h>
-#include <math.h>
 #include <stdlib.h>
 #include <gmp.h>
-
-//opção: fazer só parar quando digitar -1
 
 void limparBuffer() {
     int c;
     while ((c = getchar()) != '\n' && c != EOF);
 }
 
-int calcular_primo(int n) {
-    if (n <= 1)
+int calcular_primo(mpz_t n) {
+    if (mpz_cmp_ui(n, 1) <= 0)
+        return 0; 
+    if (mpz_cmp_ui(n, 2) == 0 || mpz_cmp_ui(n, 3) == 0)
+        return 1; 
+    if (mpz_even_p(n) || mpz_divisible_ui_p(n, 3))
         return 0;
-    if (n == 2 || n == 3)
-        return 1;
-    if (n % 2 == 0 || n % 3 == 0)
-        return 0;
-    for (int i = 5; i * i <= n; i += 6) {
-        if (n % i == 0 || n % (i + 2) == 0)
-            return 0;
-    }
-    return 1;
-}
 
-int algoritmo_euclides(int a, int b) {
-    while (b != 0) {
-        int resto = a % b;
-        a = b;
-        b = resto;
-    }
-    return a;
-}
-
-int algoritmo_euclides_estendido(int a, int b, int *s, int *t) {
-    if (b == 0) {
-        *s = 1;
-        *t = 0;
-        return a;
-    }
-
-    int s1, t1;
-    int mdc = algoritmo_euclides_estendido(b, a % b, &s1, &t1);
-
-    *s = t1;
-    *t = s1 - (a / b) * t1;
-
-    return mdc;
-}
-
-long long int exponenciacao_modular(long long int base, long long int exp, long long int mod) {
-    long long int resultado = 1;
-    base = base % mod;
-    while (exp > 0) {
-        if (exp % 2 == 1) {
-            resultado = (resultado * base) % mod;
-        }
-        exp = exp >> 1;
-        base = (base * base) % mod;
-    }
-    return resultado;
+    return mpz_probab_prime_p(n, 25); 
 }
 
 int main() {
-    printf("Escolha:\n 1 - Gerar chave publica\n 2 - Encriptar\n 3 - Desencriptar\n");
+    printf("Escolha 1 (Gerar chave publica), 2 (Encriptar) ou 3 (Desencriptar):\n");
     int escolha;
-    long int p, q, e;
-    long long int n, produtopq;
+    mpz_t n, produtopq, p, q, e, d, t, p1, q1, mdc;
+    mpz_inits(n, produtopq, p, q, e, d, t, p1, q1, mdc, NULL); 
+
     scanf("%d", &escolha);
 
     switch (escolha) {
         case 1: // Gerar chave pública
             printf("Digite P, Q, e E:\n");
-            scanf("%ld %ld %ld", &p, &q, &e);
+            gmp_scanf("%Zd %Zd %Zd", p, q, e);
+
             // Checar se p e q são primos
             while (calcular_primo(p) == 0 || calcular_primo(q) == 0) {
-                printf("P e Q nao sao primos! \nInsira P, Q e E novamente:\n");
-                scanf("%ld %ld %ld", &p, &q, &e);
+                printf("P e Q nao sao primos! Insira P, Q e E novamente:\n");
+                gmp_scanf("%Zd %Zd %Zd", p, q, e);
             }
-            produtopq = (p - 1) * (q - 1);
-            // Checar se e é coprimo ao produto (p-1)*(q-1)
-            while (algoritmo_euclides(e, produtopq) != 1) {
-                printf("e nao eh coprimo ao produto (p-1)*(q-1)!\n Insira P, Q e E novamente:\n");
-                scanf("%ld %ld %ld", &p, &q, &e);
+
+            mpz_sub_ui(p1, p, 1);
+            mpz_sub_ui(q1, q, 1);
+            mpz_mul(produtopq, p1, q1);
+
+            while (1) {
+                mpz_gcd(mdc, e, produtopq);
+                if (mpz_cmp_ui(mdc, 1) == 0) break;
+
+                printf("e nao eh coprimo ao produto (p-1)*(q-1)! Insira um novo E:\n");
+                gmp_scanf("%Zd", e);
             }
-            n = p * q;
+
+            mpz_mul(n, p, q);
             FILE *pont_arq = fopen("chave_publica.txt", "w");
             if (pont_arq != NULL) {
-                fprintf(pont_arq, "%lld %ld\n", n, e);
+                gmp_fprintf(pont_arq, "%Zd %Zd\n", n, e);
                 fclose(pont_arq);
                 printf("Chave publica gerada e salva no arquivo 'chave_publica.txt'.\n");
             } else {
@@ -97,32 +62,32 @@ int main() {
 
         case 2: // Encriptar
             printf("Digite N e E:\n");
-            scanf("%lld %ld", &n, &e);
-
-            printf("Escolha:\n 1 - Digitar mensagem\n 2 - Fazer upload da mensagem\n");
+            gmp_scanf("%Zd %Zd", n, e);
+            printf("Escolha 1 (Digitar mensagem a encriptar) ou 2 (Fazer upload da mensagem a encriptar):\n");
             int opcao;
-            limparBuffer();
             scanf("%d", &opcao);
             limparBuffer();
 
             switch (opcao) {
-                case 1: { // Digitar mensagem
+                case 1: {
                     char letra;
-                    FILE *mensagem_crpt = fopen("mensagem_encriptografada.txt", "w+");
-                    printf("Digite a mensagem de texto a encriptar:\n");
+                    FILE *mensagem_crpt = fopen("mensagem_encriptada.txt", "w+");
+                    printf("Digite a mensagem de texto a encriptar e pressione Enter para enviar:\n");
                     while ((letra = getchar()) != '\n') {
-                        long long int encriptada = exponenciacao_modular(letra, e, n);
-                        fprintf(mensagem_crpt, "%lld ", encriptada);
+                        mpz_t encriptada;
+                        mpz_init(encriptada);
+                        mpz_set_ui(encriptada, (unsigned char)letra);
+                        mpz_powm(encriptada, encriptada, e, n);
+                        gmp_fprintf(mensagem_crpt, "%Zd ", encriptada);
+                        mpz_clear(encriptada);
                     }
                     fclose(mensagem_crpt);
                     break;
                 }
-
-                case 2: { // Upload da mensagem
+                case 2: {
                     char texto;
-                    long long int encriptada;
                     FILE *arquivoEntrada = fopen("arquivo.txt", "r");
-                    FILE *arquivoSaida = fopen("mensagem_encriptografada.txt", "w");
+                    FILE *arquivoSaida = fopen("mensagem_encriptada.txt", "w");
 
                     if (arquivoEntrada == NULL || arquivoSaida == NULL) {
                         printf("Erro ao abrir o arquivo.\n");
@@ -130,84 +95,114 @@ int main() {
                     }
 
                     while ((texto = fgetc(arquivoEntrada)) != EOF) {
-                        long long int mensagem = (long long int) texto;
-                        encriptada = exponenciacao_modular(mensagem, e, n);
-                        fprintf(arquivoSaida, "%lld ", encriptada);
+                        mpz_t mensagem, encriptada;
+                        mpz_init_set_ui(mensagem, (unsigned char)texto);
+                        mpz_init(encriptada);
+                        mpz_powm(encriptada, mensagem, e, n);
+                        gmp_fprintf(arquivoSaida, "%Zd ", encriptada);
+                        mpz_clear(mensagem);
+                        mpz_clear(encriptada);
                     }
                     fclose(arquivoEntrada);
                     fclose(arquivoSaida);
                     break;
                 }
-
                 default:
                     printf("Escolha invalida!\n");
                     break;
             }
             break;
 
-        case 3: // Desencriptar
-        printf("Digite P, Q, e E:\n");
-        scanf("%ld %ld %ld", &p, &q, &e);
-
-        printf("Escolha:\n 1 - Digitar mensagem\n 2 - Fazer upload da mensagem\n");
-        int escolher;
-        limparBuffer();
-        scanf("%d", &escolher);
-        limparBuffer();
-
-        produtopq = (p - 1) * (q - 1);
-        int d, t;
-        algoritmo_euclides_estendido(e, produtopq, &d, &t);
-        if (d < 0) {
-            d += produtopq;
-        }        
-        long long int n = p * q;
-        printf("Chave privada (D,E): (%d,%ld)\n", d, e);
-
-        long long int numero;
-
-        switch (escolher) {
-            case 1: { // Digitar mensagem
-            long long int numero;
-            FILE *mensagem_desencripitada = fopen("mensagem_desencriptografada.txt", "w+");
-            while (scanf("%lld", &numero) == 1 && numero != -1) { 
-                long long int desencriptado = exponenciacao_modular(numero, d, n);
-                fprintf(mensagem_desencripitada, "%c", (char)desencriptado);
-                printf("%c", (char)desencriptado);
-            }
-            fclose(mensagem_desencripitada);
             break;
+
+        case 3: // Desencriptar
+            printf("Digite P, Q, e E:\n");
+            gmp_scanf("%Zd %Zd %Zd", p, q, e);
+
+            mpz_sub_ui(p1, p, 1);
+            mpz_sub_ui(q1, q, 1);
+            mpz_mul(produtopq, p1, q1);
+
+            mpz_invert(d, e, produtopq);
+
+            if (mpz_cmp_ui(d, 0) < 0) {
+                mpz_add(d, d, produtopq);
             }
 
-            case 2: { //Upload mensagem
+            mpz_mul(n, p, q);
 
-                long long int numero;
-                long long int desencriptada;
-                FILE *arquivoEntrada = fopen("arquivo.txt", "r");
-                FILE *arquivoSaida = fopen("mensagem_desencriptografada.txt", "w");
+            gmp_printf("Chave privada (D,E): (%Zd, %Zd)\n", d, e);
+            printf("Escolha 1 (Digitar mensagem a desencriptar) ou 2 (Fazer upload da mensagem a desencriptar):\n");
+            int escolher;
+            scanf("%d", &escolher);
+            limparBuffer();
 
-                if (arquivoEntrada == NULL || arquivoSaida == NULL) {
-                    printf("Erro ao abrir o arquivo.\n");
+            switch (escolher) {
+                case 1: {
+                    printf("Digite a mensagem a desencriptar e digite -1 ao final para enviar:\n");
+                    mpz_t numero_encriptado;
+                    mpz_init(numero_encriptado);
+                    FILE *mensagem_desencriptada = fopen("mensagem_desencriptografada.txt", "w+");
+                     while (1) { 
+                    gmp_scanf("%Zd", numero_encriptado);
+                    if (mpz_cmp_si(numero_encriptado, -1) == 0) break;
+                    
+                    mpz_t desencriptado;
+                    mpz_init(desencriptado);
+                    mpz_powm(desencriptado, numero_encriptado, d, n);
+
+                    // Convertendo para caractere ASCII
+                    unsigned long char_val = mpz_get_ui(desencriptado);
+                    fprintf(mensagem_desencriptada, "%c", (char)char_val);
+                    printf("%c", (char)char_val);
+
+                    mpz_clear(desencriptado);
+                }
+                mpz_clear(numero_encriptado);
+                fclose(mensagem_desencriptada);
+                break;
+}
+
+                case 2: {
+                    mpz_t numero;
+                    mpz_init(numero);
+                    FILE *arquivoEntrada = fopen("arquivo.txt", "r");
+                    FILE *arquivoSaida = fopen("mensagem_desencriptografada.txt", "w");
+
+                    if (arquivoEntrada == NULL || arquivoSaida == NULL) {
+                        printf("Erro ao abrir o arquivo.\n");
+                        break;
+                    }
+
+                    while (gmp_fscanf(arquivoEntrada, "%Zd", numero) != EOF) { 
+                    mpz_t desencriptado;
+                    mpz_init(desencriptado);
+                    mpz_powm(desencriptado, numero, d, n);
+
+                    // Convertendo para caractere ASCII
+                    unsigned long char_val = mpz_get_ui(desencriptado);
+                    fprintf(arquivoSaida, "%c", (char)char_val);
+                    printf("%c", (char)char_val); // opcional para verificação
+
+                    mpz_clear(desencriptado);
+                }
+                    mpz_clear(numero);
+                    fclose(arquivoEntrada);
+                    fclose(arquivoSaida);
                     break;
                 }
-
-                while (fscanf(arquivoEntrada, "%lld", &numero) == 1 && numero != -1) { 
-                long long int desencriptado = exponenciacao_modular(numero, d, n);
-                fprintf(arquivoSaida, "%c", (char)desencriptado); 
-                }
-                fclose(arquivoEntrada);
-                fclose(arquivoSaida);
+                default:
+                    printf("Escolha invalida!\n");
                     break;
-
             }
-        }
+            mpz_clears(p1, q1, NULL);
+            break;
 
-        break;
-
-    default:
-        printf("Escolha invalida!\n");
-        break;
+        default:
+            printf("Escolha invalida!\n");
+            break;
     }
 
+    mpz_clears(n, produtopq, p, q, e, d, t, p1, q1, mdc, NULL); 
     return 0;
 }
